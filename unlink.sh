37ad2e6a7f3cc7cf027253dbcb25a438b7b70a0b
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2022-11-01 15:51:40 +0100

    improve messages in unlink script

--- unlink.sh
+++ unlink.sh
@@ -10,12 +10,13 @@ function doIt() {
 
 if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
 	doIt;
+	echo "Please logout from your shell (CTRL+D or type 'exit') for the changes to be applied."
 else
 	read -rp "This will unlink the dotfiles from your home directory. No files will actually be deleted. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
-		echo "Please logout from your shell (CTRL+D) for the changes to be applied."
+		echo "Please logout from your shell (CTRL+D or type 'exit') for the changes to be applied."
 	fi;
 fi;
 unset doIt;
