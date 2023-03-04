499ed7f70a10fab56730c84a97bed7c38169add7
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2023-03-04 22:06:22 +0100

    fix linting errors

--- macos.sh
+++ macos.sh
@@ -768,8 +768,8 @@ tell application "Terminal"
 	set initialOpenedWindows to id of every window
 
 	(* Open the custom theme so that it gets added to the list
-	   of available terminal themes (note: this will open two
-	   additional terminal windows). *)
+		of available terminal themes (note: this will open two
+		additional terminal windows). *)
 	do shell script "open '$script_dir/init/" & themeName & ".terminal'"
 
 	(* Wait a little bit to ensure that the custom theme is added. *)
@@ -784,13 +784,13 @@ tell application "Terminal"
 	repeat with windowID in allOpenedWindows
 
 		(* Close the additional windows that were opened in order
-		   to add the custom theme to the list of terminal themes. *)
+			to add the custom theme to the list of terminal themes. *)
 		if initialOpenedWindows does not contain windowID then
 			close (every window whose id is windowID)
 
 		(* Change the theme for the initial opened terminal windows
-		   to remove the need to close them in order for the custom
-		   theme to be applied. *)
+			to remove the need to close them in order for the custom
+			theme to be applied. *)
 		else
 			set current settings of tabs of (every window whose id is windowID) to settings set themeName
 		end if
