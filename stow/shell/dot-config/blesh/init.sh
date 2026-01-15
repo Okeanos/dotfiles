c1b1c9f04543625112bc81c6d2358d701cd6d8fb
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2026-01-15 18:50:04 +0100

    fix: correct interaction between ble.sh and Ghostty
    See https://github.com/akinomyoga/ble.sh/issues/662 for details.

--- stow/shell/dot-config/blesh/init.sh
+++ stow/shell/dot-config/blesh/init.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 # the shebang statement above is only here to allow linting this file
+
+ble/function#advice before ble/term/modifyOtherKeys/.supported 'method=kitty_keyboard_protocol'
