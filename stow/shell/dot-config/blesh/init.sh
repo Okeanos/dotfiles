9f66f76282ed56c0dc204410d26994189167422c
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2026-06-21 20:39:21 +0200

    fix: remove obsolete keyboard support workaround in ble.sh config
    With https://github.com/akinomyoga/ble.sh/commit/6cffa910eccce252fa391875c7c03c94fb6f1b09
    a fix was introduced in ble.sh that provides direct Ghostty support for ble.sh
    and makes keys work as expected without manually setting ble.sh to kitty
    keyboard protocol mode.
    
    See https://github.com/akinomyoga/ble.sh/issues/662

--- stow/shell/dot-config/blesh/init.sh
+++ stow/shell/dot-config/blesh/init.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
 # the shebang statement above is only here to allow linting this file
-
-ble/function#advice before ble/term/modifyOtherKeys/.supported 'method=kitty_keyboard_protocol'
